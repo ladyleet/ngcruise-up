0fa519261f00776ef1d4e7c4ba863542ce8eb57b
Tracy Lee <tracy@wingman.events>
2016-11-16 09:59:03 -0500

    initial setup

---
+++ src/app/speakers/speakers.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-speakers',
+  template: `
+    <p>
+      speakers Works!
+    </p>
+  `,
+  styles: []
+})
+export class SpeakersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
