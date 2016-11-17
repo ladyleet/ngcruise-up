a369637505731ce420654aaf6666d320caa97752
Tracy Lee <tracy@wingman.events>
2016-11-16 19:13:40 -0500

    another proper one

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -7,19 +7,13 @@ import { Component, OnInit } from '@angular/core';
       <div class="nav-wrapper light-blue">
         <a href="#" class="brand-logo center"><img src="/assets/logo.png"/></a>
         <ul id="nav-mobile" class="right hide-on-med-and-down">
-          <li><a href="#">temp</a></li>
-          <li><a href="#">temp</a></li>
-          <li><a href="#">temp</a></li>
+          <li><a routerLink="about">About</a></li>
+          <li><a routerLink="speakers">Speakers</a></li>
+          <li><a routerLink="sponsor">Sponsor</a></li>
         </ul>
       </div>
     </nav>
-    <img class="image-stretch" src="/assets/header.png"/>
-  `,
-  styles: [`
-    .image-stretch {
-      width:100%; height:100%;
-    }
-    `]
+  `
 })
 export class NavBarComponent implements OnInit {
 
--- src/app/speaker-card/speaker-card.component.ts
+++ src/app/speaker-card/speaker-card.component.ts
@@ -3,18 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-speaker-card',
   template: `
-    <div class="col s12 m7">
-      <h2 class="header">Horizontal Card</h2>
+    <div *ngFor="let speaker of speakers" class="col s12">
       <div class="card horizontal">
         <div class="card-image">
-          <img src="http://lorempixel.com/100/190/nature/6">
+          <img src="/assets/speakers/{{speaker.image}}">
         </div>
         <div class="card-stacked">
           <div class="card-content">
-            <p>I am a very simple card. I am good at containing small bits of information.</p>
-          </div>
-          <div class="card-action">
-            <a href="#">This is a link</a>
+            <h2>{{speaker.name}}</h2>
+            <h4>{{speaker.topic}}</h4>
           </div>
         </div>
       </div>
@@ -23,6 +20,52 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class SpeakerCardComponent implements OnInit {
+
+  speakers: Object=[] = [{
+    name: "Aaron Frost",
+    image: "aaron-frost.jpg",
+    topic: "CaaS: Components as a Service"
+  },
+  {
+    name: "Alyssa Nicoll",
+    image: "alyssa-nicoll.jpg",
+    topic: "Animations in an Angular World"
+  },
+  {
+    name: "Ben Lesh",
+    image: "ben-lesh.jpg",
+    topic: "RxJS by Example"
+  },
+  {
+    name: "Gerard Sans",
+    image: "gerard-sans.jpg",
+    topic: "Panel Moderator"
+  },
+  {
+    name: "Jeff Cross",
+    image: "jeff-cross.jpg",
+    topic: "Keynote"
+  },
+  {
+    name: "Joe Eames",
+    image: "joe-eames.jpeg",
+    topic: "Panel Moderator"
+  },
+  {
+    name: "Mike Brocchi",
+    image: "mike-brocchi.jpg",
+    topic: "Angular-CLI"
+  },
+  {
+    name: "Tracy Lee",
+    image: "tracy-lee.jpg",
+    topic: "Hostess"
+  },
+  {
+    name: "Victor Savkin",
+    image: "victor-savkin.jpg",
+    topic: "Keynote"
+  }]
 
   constructor() { }
 
