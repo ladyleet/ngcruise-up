c54aefe8e70dc83c037feb3040b02e32c962f1db
Tracy Lee <tracy@wingman.events>
2016-11-21 16:55:30 -0500

    angular-up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
   <app-nav-bar></app-nav-bar>
+  <router-outlet></router-outlet>
   `,
   styles: []
 })
--- src/app/speakers/speakers.component.ts
+++ src/app/speakers/speakers.component.ts
@@ -3,9 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-speakers',
   template: `
-    <p>
-      speakers Works!
-    </p>
+  <div class="row">
+    <div class="col s12">
+      <h2 class="center-align">Meet the Speakers</h2>
+    </div>
+  </div>
+  <div class="row">
+    <app-speaker-card></app-speaker-card>
+  </div>
+
   `,
   styles: []
 })
