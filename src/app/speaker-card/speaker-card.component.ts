18ee6d96821778c15d91a29061f3d6206e38f72d
Tracy Lee <tracy@wingman.events>
2016-11-22 08:12:37 -0500

    final

--- src/app/speaker-card/speaker-card.component.ts
+++ src/app/speaker-card/speaker-card.component.ts
@@ -3,15 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-speaker-card',
   template: `
-    <div class="col s12">
+    <div *ngFor="let speaker of speakers" class="col s12">
       <div class="card horizontal">
         <div class="card-image">
-          <img src="http://lorempixel.com/100/190/nature/6">
+          <img src="/assets/speakers/{{speaker.image}}">
         </div>
         <div class="card-stacked">
           <div class="card-content">
-            <h2>Speaker Name</h2>
-            <h4>Talk Title</h4>
+            <h2>{{speaker.name}}</h2>
+            <h4>{{speaker.topic}}</h4>
           </div>
         </div>
       </div>
@@ -20,6 +20,52 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class SpeakerCardComponent implements OnInit {
+
+  speakers: Object=[] = [{
+      name: "Aaron Frost",
+      image: "aaron-frost.jpg",
+      topic: "CaaS: Components as a Service"
+    },
+    {
+      name: "Alyssa Nicoll",
+      image: "alyssa-nicoll.jpg",
+      topic: "Animations in an Angular World"
+    },
+    {
+      name: "Ben Lesh",
+      image: "ben-lesh.jpg",
+      topic: "RxJS by Example"
+    },
+    {
+      name: "Gerard Sans",
+      image: "gerard-sans.jpg",
+      topic: "Panel Moderator"
+    },
+    {
+      name: "Jeff Cross",
+      image: "jeff-cross.jpg",
+      topic: "Keynote"
+    },
+    {
+      name: "Joe Eames",
+      image: "joe-eames.jpeg",
+      topic: "Panel Moderator"
+    },
+    {
+      name: "Mike Brocchi",
+      image: "mike-brocchi.jpg",
+      topic: "Angular-CLI"
+    },
+    {
+      name: "Tracy Lee",
+      image: "tracy-lee.jpg",
+      topic: "Hostess"
+    },
+    {
+      name: "Victor Savkin",
+      image: "victor-savkin.jpg",
+      topic: "Keynote"
+    }]
 
   constructor() { }
 
---
+++ src/app/sponsor/sponsor.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sponsor',
+  template: `
+  <div class="row">
+    <div class="col s12">
+      <h2 class="center-align">Sponsor Ng-Cruise</h2>
+    </div>
+  </div>
+  <div class="row">
+    <form #myForm="ngForm" (ngSubmit) = "submitForm(myForm)" class="col s12">
+      <div class="row">
+        <div class="input-field col s6">
+          <input [(ngModel)] = "submissions.firstName" name="first_name" id="first_name" type="text" class="validate">
+          <label for="first_name">First Name</label>
+        </div>
+        <div class="input-field col s6">
+          <input [(ngModel)] = "submissions.lastName" name="last_name" id="last_name" type="text" class="validate">
+          <label for="last_name">Last Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input [(ngModel)] = "submissions.email" name="email" id="email" type="email" class="validate">
+          <label for="email">Email</label>
+        </div>
+      </div>
+      <button class="btn waves-effect waves-light light-blue" type="submit" name="action">Submit
+        <i class="material-icons right">send</i>
+      </button>
+    </form>
+  </div>
+  <div *ngIf="results" class="row">
+    <div class="col s12">
+      <h4>Hi {{results.first_name}}, thanks for wanting to sponsor ng-cruise!</h4>
+      <h4>To confirm, your first name is "{{results.first_name}}", your last name is "{{results.last_name}}", and your email is "{{results.email}}"</h4>
+    </div>
+  </div>
+  `,
+  styles: []
+})
+export class SponsorComponent implements OnInit {
+  submitForm(myForm:NgForm) {
+    console.log(myForm);
+    this.results = myForm.value;
+  }
+
+  submissions: Submissions = { firstName: '', lastName: '', email: '' };
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
